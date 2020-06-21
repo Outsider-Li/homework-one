a09dda64d181a29d6959690f7d9670ce009dc0f6
Outsider-Li <964248839@qq.com>
2020-06-21 17:38:08 +0800

    add code

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+	
+	public static int of (int num) {
+		if(num == 1 || num == 2) {
+			return 1;
+		}
+		return of(num-1) + of(num-2);
+	}
+		
+	
+	public static void main(String[] args) {
+		for(int i=1; i<=12; i++) {
+			System.out.println("Fibonacci.of(" + i + ") == " +Fibonacci.of(i));
+		}
+	}
+}
